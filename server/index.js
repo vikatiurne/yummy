421042a8ba72452d52156cfec64dbfb770534f24
vikatiurne <viktoriya83-08@ukr.net>
2023-11-17 14:29:42 +0200

    chenchd host db

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import sequelize from './bd.js';
 import router from './routes/index.js';
 import errorHandler from './middleware/ErrorHandlingMiddleware.js';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(
